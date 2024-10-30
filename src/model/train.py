b31e492b4693cb7c82083ccb4400c75c72dc53a5
Melissa Tjhia <melissa@tjhia.com>
2024-10-30 16:07:54 +0100

    fix indentation

--- src/model/train.py
+++ src/model/train.py
@@ -37,8 +37,8 @@ def get_csvs_df(path):
 
 def split_data(df):
     X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure',
-        'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree',
-        'Age']].values, df['Diabetic'].values
+               'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree',
+               'Age']].values, df['Diabetic'].values
     X_train, X_test, y_train, y_test = \
         train_test_split(X, y, test_size=0.30, random_state=0)
 
